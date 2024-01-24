8b4a71a58ed0f485bcc0087502e4ecc64aaccabb
Orçun Çolak <orcun.colak@hazelcast.com>
2024-01-24 12:58:06 +0300

    Use bom for jackson

--- src/main/java/xml/load/XmlConfigurationTest.java
+++ src/main/java/xml/load/XmlConfigurationTest.java
@@ -3,22 +3,20 @@ package xml.load;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-public class XmlConfiguration {
+public class XmlConfigurationTest {
 
     private final Map<String, Concept> conceptMap = new HashMap<>();
 
     public static void main(String[] args) {
-        XmlConfiguration xmlConfiguration = new XmlConfiguration();
-        xmlConfiguration.loadXmlData();
-        log.info("Loaded XML {}", xmlConfiguration.conceptMap);
+        XmlConfigurationTest xmlConfigurationTest = new XmlConfigurationTest();
+        xmlConfigurationTest.loadXmlData();
+        log.info("Loaded XML {}", xmlConfigurationTest.conceptMap);
     }
 
     public void loadXmlData() {
