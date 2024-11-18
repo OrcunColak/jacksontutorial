ddabaab95a56da312d117232e867338367b15d8f
Orçun Çolak <orcun.colak@hazelcast.com>
2024-11-18 09:38:14 +0300

    streaming api

---
+++ src/main/java/streaming/ProductsParser.java
@@ -0,0 +1,74 @@
+package streaming;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.type.TypeReference;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+// See https://medium.com/@vino7tech/efficient-large-json-processing-in-spring-boot-using-jackson-streaming-api-a19366aa4942
+// The streaming API (JsonParser) processes JSON tokens one by one without loading the entire file into memory, making it ideal for large data.
+@Slf4j
+class ProductsParser {
+
+    @Getter
+    @Setter
+    @ToString
+    private static class Product {
+        private int id;
+        private String name;
+        private double price;
+    }
+
+    public static void main(String[] args) {
+
+        List<Product> matchingProducts = new ArrayList<>();
+
+        try (InputStream inputStream = TypeReference.class.getResourceAsStream("/json/products.json")) {
+
+            try (JsonParser jsonParser = new JsonFactory().createParser(inputStream)) {
+                // Start parsing JSON
+                if (jsonParser.nextToken() == JsonToken.START_ARRAY) {
+                    while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
+                        Product product = readProduct(jsonParser);
+
+                        // Filter logic - Example: Only process products with price > 100
+                        if (product.getPrice() > 100) {
+                            matchingProducts.add(product);
+                        }
+                    }
+                }
+            }
+        } catch (IOException exception) {
+            log.error("Exception ", exception);
+        }
+        log.info("Product List : {}", matchingProducts);
+    }
+
+    // The Product objects are deserialized incrementally.
+    // Only those matching certain criteria (e.g., price > 100) are stored in memory and returned, reducing memory usage.
+    private static Product readProduct(JsonParser jsonParser) throws IOException {
+        Product product = new Product();
+        while (jsonParser.nextToken() != JsonToken.END_OBJECT) {
+            String fieldName = jsonParser.currentName();
+            jsonParser.nextToken(); // move to value
+
+            if ("id".equals(fieldName)) {
+                product.setId(jsonParser.getIntValue());
+            } else if ("name".equals(fieldName)) {
+                product.setName(jsonParser.getText());
+            } else if ("price".equals(fieldName)) {
+                product.setPrice(jsonParser.getDoubleValue());
+            }
+        }
+        return product;
+    }
+}
